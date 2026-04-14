88d3a40efd5421fb632f6139f8495f0e6d41e158
catalogonacional2016-max <catalogonacional2016@gmail.com>
2026-04-14 11:01:26 -0300

    app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ st.title("Análise de Produtos Copa Nacional")
 st.markdown("""
 ### 📌 Categorias
 
-💰 Top Faturamento → maior valor faturado nos últimos 3 meses + maior valor faturado no mês atual
-🔥 Produto da Vez → maior valor faturado no mês anterior + maior valor faturado no mês atual
-💵 Oportunidade → maior nº de pedidos nos últimos 3 meses + baixa quantidade
+💰 Top Faturamento → maior valor faturado nos últimos 3 meses + maior valor faturado no mês atual  
+🔥 Produto da Vez → maior valor faturado no mês anterior + maior valor faturado no mês atual  
+💵 Oportunidade → maior nº de pedidos nos últimos 3 meses + baixa quantidade  
 """)
 
 # =========================
